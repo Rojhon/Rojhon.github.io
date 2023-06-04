3bc129050f5efe9213d03476529bb0b8ca59d4d1
Rojhon <rojhonbuenaventura5@gmail.com>
2023-06-03 21:15:48 -0700

    Build

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,8 +10,8 @@
 
 	<pre>Email: rojhonbuenaventura5@gmail.com</pre>
 	<pre>Phone Number: 09763443914 | 09263866697</pre>
-	<pre>LinkedIn: <a href=" https://www.linkedin.com/in/rojhon-buenaventura-684577229/" target="_blank">Click Me!</a></pre>
-	<pre>Facebook: <a href="https://www.facebook.com/rojhon.buenaventura.9/" target="_blank">Click Me!</a></pre>
+	<!-- <pre>LinkedIn: <a href=" https://www.linkedin.com/in/rojhon-buenaventura-684577229/" target="_blank">Click Me!</a></pre>
+	<pre>Facebook: <a href="https://www.facebook.com/rojhon.buenaventura.9/" target="_blank">Click Me!</a></pre> -->
 
 	<p>
 		Thank you for considering reaching out to me! I value your time and interest. I will make it a priority to respond to your message as soon as possible. Meanwhile, feel free to explore the rest of my portfolio website to learn more about my work and skills. I'm excited to connect with you and explore potential opportunities. Have a great day!
